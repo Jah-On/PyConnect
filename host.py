02622ae7d4dc9ad1fdbc3df4fe5b8b94eb27b971
Jah-On <58399643+Jah-On@users.noreply.github.com>
2020-06-29 22:30:58 -0500

    Updated for theoretical complete OS intercompatibilty!

--- host.py
+++ host.py
@@ -1,10 +1,9 @@
-import socket
 import pynput
 import io
 from pynput import mouse, keyboard
 from pynput.keyboard import Key, Controller
 from pynput.mouse import Button, Controller
-import os
+import fs
 import sys
 import time
 import pyscreenshot as ImageGrab
@@ -12,9 +11,13 @@ import threading
 from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
+import logging
 
-os.system("mkdir Temp")
-os.system("sudo mount -t tmpfs -o rw,size=50M tmpfs Temp")
+mem = fs.open_fs('mem://')
+
+# Thanks Giampaolo Rodolà of stackoverflow.com
+# from pyftpdlib.log import config_logging
+# config_logging(level=logging.ERROR)
 
 print('Enter IP address or Hostname this device should use.')
 ip = input()
@@ -25,10 +28,6 @@ keyboard = pynput.keyboard.Controller()
 # From pyftpdlib documentation with minor changes
 # Instantiate a dummy authorizer for managing 'virtual' users
 authorizer = DummyAuthorizer()
-
-# Define a new user having full r/w permissions and a read-only
-# anonymous user
-authorizer.add_user('user', '12345', 'Temp', perm='elradfmwMT')
 
 # Instantiate FTP handler class
 handler = FTPHandler
@@ -45,88 +44,100 @@ server = FTPServer(address, handler)
 
 # set a limit for connections
 server.max_cons = 2
-server.max_cons_per_ip = 2
+server.max_cons_per_ip = 1
 
+online = True
+gtg = False
+telller1File = mem.open('teller_h.txt', 'rw')
+recievedTellerFile = mem.open("recvedTeller.txt", 'rw')
+recievedTellerFile.write('1')
+telller1File.close()
+recievedTellerFile.close()
 
-s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s2.bind((str(ip), 1235))
-s2.listen(1)
+def Bye():
+    global online
+    while online:
+        try:
+            # Session status checker
+            if splited[0] == '1':
+                print('Closing input thread')
+                online = False
+                gtg = False
+                os.system('sudo umount Temp')
 
-connection2, addr2 = s2.accept()
+        except:
+            pass
+
+def FTP_Start():
+    server.serve_forever()
 
 def Output():
     global online
     global splited
-    online = True
     while online:
-        image = ImageGrab.grab()
-        image.save("Temp/image.jpg")
-        teller = '1'
-        connection2.send(teller.encode('utf-8'))
-        time.sleep(.0166)
-        if splited[0] == '1':
-            server.close_all()
-            os.system('sudo umount Temp')
-            break
+        recievedTellerFile = mem.open("recvedTeller.txt", 'rw')
+
+        if str(recievedTellerFile.read()) == "1":
+            recievedTellerFile.write('0')
+            image = ImageGrab.grab()
+            image.mem.save("image.jpg")
+            teller1 = '1'
+            telller1File = mem.open('Temp/teller_h.txt', 'rw')
+            telller1File.write('1')
+            telller1File.close()
+
+        recievedTellerFile.close()
 
 def Input():
     global online
     global splited
+    global gtg
     while online:
-        # Data handlers
-        get = connection2.recv(1500)
-        get_decoded = get.decode("utf-8")
-        splited = get_decoded.split(":", 50)
+        while gtg:
 
-        # Session status checker
-        if splited[0] == '1':
-            print('Closing input thread')
-            server.close_all()
-            break
+            # Mouse handlers
+            def mousectl():
+                global online
+                global gtg
+                try:
+                    mosx = int(splited[1])
+                    mosy = int(splited[2])
+                    mouse.position(mosx, mosy)
+                    if splited[3] == "1":
+                        mouse.press(Button.left)
+                    else:
+                        mouse.release(Button.left)
+                    if splited[4] == "1":
+                        mouse.press(Button.right)
+                    else:
+                        mouse.release(Button.right)
 
-        # Mouse handlers
-        posx = float(splited[1])
-        posy = float(splited[2])
-        mouse.position = (posx, posy)
-        if splited[3] == "1":
-            mouse.press(Button.left)
-        else:
-            mouse.release(Button.left)
-        if splited[4] == "1":
-            mouse.press(Button.right)
-        else:
-            mouse.release(Button.right)
+                except:
+                    pass
 
-        # Keyboard handlers
-        if splited[5] == "1":
-            keyboard.press('0')
-        else:
-            keyboard.release("0")
-        if splited[6] == "1":
-            keyboard.press('1')
-        else:
-            keyboard.release('1')
-        if splited[7] == "1":
-            keyboard.type('2')
-        if splited[8] == "1":
-            keyboard.type('3')
-        if splited[9] == "1":
-            keyboard.type('4')
-        if splited[10] == "1":
-            keyboard.type('5')
-        if splited[11] == "1":
-            keyboard.type('6')
-        if splited[12] == "1":
-            keyboard.type('7')
-        if splited[13] == "1":
-            keyboard.type('8')
-        if splited[14] == "1":
-            keyboard.type('9')
+            mousectl()
 
+def kb_top0():
+    global gtg
+    global splited
+    time.sleep(.01)
+    while online:
+        while gtg:
+            try:
+                while splited[5] == "1":
+                    keyboard.type('0')
+                    time.sleep(.05)
+
+            except:
+                pass
 
 # Define threads
+Bye_thread = threading.Thread(target=Bye)
 output_thread = threading.Thread(target=Output)
 input_thread = threading.Thread(target=Input)
+kb_top0_thread = threading.Thread(target=kb_top0)
+FTP_Start_thread = threading.Thread(target=FTP_Start)
 output_thread.start()
-input_thread.start()
-server.serve_forever()
+FTP_Start_thread.start()
+# Bye_thread.start()
+quit()
